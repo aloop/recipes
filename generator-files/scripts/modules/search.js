0404ab57a4d1f1a3fa61018dcf3039730e366a87
Anthony Loop <anthony@anthonyloop.com>
2019-08-13 20:36:47 -0700

    Lower debounce delay for search input

--- generator-files/scripts/modules/search.js
+++ generator-files/scripts/modules/search.js
@@ -48,4 +48,4 @@ export const searchListener = ev => {
   }
 };
 
-export const createSearchListener = () => debounce(searchListener);
+export const createSearchListener = () => debounce(searchListener, 50);
