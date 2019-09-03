afbb8b829db65964dd3ca8d5892435876d5bb633
Anthony Loop <anthony@anthonyloop.com>
2019-09-02 17:12:08 -0700

    Make service worker network-first, avoid stale content

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "4";
+const CACHE_VERSION = "5";
 const OFFLINE_PAGE_URL = "/offline.html";
 const CACHES = {
   offline: `offline-v${CACHE_VERSION}`
@@ -6,51 +6,53 @@ const CACHES = {
 const CACHE_NAMES = Object.values(CACHES);
 
 const activate = async () => {
-  if (self.registration.navigationPreload) {
-    console.log("navigationPreload supported");
+  if ("navigationPreload" in self.registration) {
     await self.registration.navigationPreload.enable();
   }
 
   const cacheNames = await caches.keys();
 
-  return Promise.all(
-    cacheNames.map(cacheName => {
+  return Promise.all([
+    ...cacheNames.map(cacheName => {
       if (!CACHE_NAMES.includes(cacheName)) {
         return caches.delete(cacheName);
       }
-    })
-  );
+    }),
+    self.clients.claim()
+  ]);
 };
 
 const fetchResponder = async event => {
   const cache = await caches.open(CACHES.offline);
 
-  let response = await event.preloadResponse;
+  let response;
 
-  if (!response) {
-    try {
-      response = await fetch(event.request);
-    } catch {
-      // Fetch should only throw when it gets an invalid response of some sort,
-      // so there shouldn't be any problem showing the offline page without
-      // any real checks
-      return cache.match(OFFLINE_PAGE_URL);
+  try {
+    if ("preloadResponse" in event) {
+      response = await event.preloadResponse;
     }
+
+    if (!response) {
+      response = await fetch(event.request);
+    }
+  } catch {
+    // Fetch should only throw when it gets an invalid response of some sort,
+    // so when it does we'll try to serve the page from the cache, otherwise
+    // show our offline page.
+    return (await cache.match(event.request)) || cache.match(OFFLINE_PAGE_URL);
   }
 
-  const clonedResponse = response.clone();
-
-  // Add to the cache if the response code is in the 200 range
-  if (response.ok) {
-    cache.put(event.request, clonedResponse);
+  if (response.status === 200) {
+    const clonedResponse = response.clone();
+    await cache.put(event.request, clonedResponse);
   }
 
-  const cachedResponse = await caches.match(event.request);
-
-  return cachedResponse || response;
+  return response || cache.match(event.request);
 };
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHES.offline).then(cache => cache.addAll([OFFLINE_PAGE_URL]))
   );
